881a4c80afc65374fad8627c118fa7dc58d12af4
DylanMo2013 <127252979+DylanMo2013@users.noreply.github.com>
2025-05-13 17:57:20 -0600

    Archivo de prueba Dylan

---
+++ Colaboracion prueba G1/PruebaDylan/frm_PruebaDylan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colaboracion_prueba_G1.PruebaDylan
+{
+    public partial class frm_PruebaDylan : Form
+    {
+        public frm_PruebaDylan()
+        {
+            InitializeComponent();
+        }
+    }
+}
